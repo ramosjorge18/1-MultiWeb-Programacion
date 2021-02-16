0f32c219d539987fd1b94db5bd1bddf86f75d3b0
ramosjorge18 <ramosjorge1996@gmail.com>
2021-02-16 21:42:32 +0100

    Ejercicio2 OK

---
+++ src/main/java/tema9/ejercicio3/Racional.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tema9.ejercicio3;
+
+/**
+ *
+ * @author Vespertino
+ */
+public class Racional {
+    //Atributos
+    private int numerador;
+    private int denominador;
+    //Constructores
+    public Racional(int numerador, int denominador){
+        this.numerador = numerador;
+        this.denominador = denominador;
+    }
+    
+    //Metodos
+    
+    
+    
+    
+    
+}
