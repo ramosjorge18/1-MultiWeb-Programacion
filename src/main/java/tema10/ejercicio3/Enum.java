eab458598442479083dbf58a955e1b55f82689e6
ramosjorge18 <ramosjorge1996@gmail.com>
2021-03-16 21:12:22 +0100

    Enums

---
+++ src/main/java/tema10/ejercicio3/Enum.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tema10.ejercicio3;
+
+/**
+ *
+ * @author Vespertino
+ */
+enum DiasSemana {
+    LUNES, MARTES, MIERCOLES, JUEVES, VIERNES, SABADO, DOMINGO
+}
+
+enum Orientacion {
+    NORTE,SUR,ESTE,OESTE
+}
+
+enum Mes {
+    ENERO, FEBRERO, MARZO, ABRIL, MAYO, JUNIO, JULIO, AGOSTO, SEPTIEMBRE, OCTUBRE, NOVIEMBRE, DICIEMBRE
+}
+
+enum Genero {
+    MASCULINO, FEMENINO, NOBINARIO
+}
+
+enum PosicionesFutbol {
+    PORTERO, LATERALDERECHO, CENTRALDERECHO, CENTRALIZQUIERO, LATERALIZQUIERDO, MEDIOCENTRODEFENSIVO,
+    MEDIOCENTRO, MEDIOCENTROOFENSIVO, EXTREMODERECHO, EXTREMOIZQUIERDO, DELANTERO, CARRILERODERECHO, CARRILEROIZQUIERDO, INTERIORDERECHA, INTERIORIZQUIERDA
+}
+
+enum ArtesMarciales {
+    KICKBOXING, KARATE, K1, BOXEO, KARVMAGA, TAEKWONDO, KICKLIGHT, FULLCONTACT, JIUJITSU, MUAYTHAI, JUDO
+}
+
+        
+    
+    
+    
+    
+    
+
---
+++ src/main/java/tema10/ejercicio3/Run.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tema10.ejercicio3;
+
+/**
+ *
+ * @author Vespertino
+ */
+public class Run {
+    public static void main(String[] args) {
+        
+        DiasSemana [] a;
+        a= DiasSemana.values();       
+        for (DiasSemana diasSemana : a) {
+            System.out.println(diasSemana);
+            if(diasSemana == DiasSemana.SABADO){
+            System.out.println("FIESTA");
+            }
+        }        
+        
+        ArtesMarciales x = ArtesMarciales.valueOf("KICKBOXING");
+        System.out.println(x);
+        
+        x= ArtesMarciales.BOXEO;
+        
+        switch (x) {
+            case BOXEO:
+                System.out.println("SOLO PUEDES PEGAR CON LAS MANOS");
+                break;
+            case JIUJITSU:
+                System.out.println("TOCA HACER LLAVES");
+                break;
+            case MUAYTHAI:
+                System.out.println("A LO TAILANDES");
+                
+        }
+                
+            
+        
+        
+    }
+    
+}
